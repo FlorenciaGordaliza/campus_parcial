3d211a78c47ce476ab2e5a572f58ea3954bba793
florencia <florenciiaailen2002@gmail.com>
2025-12-03 13:21:51 -0300

    arreglos

--- campus/settings.py
+++ campus/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-!6p(n!xp=2++s)jlom5)jt7k^4t(=^nx_n-yxjyc7(k)y-&r$z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
